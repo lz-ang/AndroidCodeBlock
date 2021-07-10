fe5ddc5950614f677c8109985f0b9d6ffbffc46b
lza <liuzhiang@gtland.cn>
2021-07-10 18:54:55 +0800

    阻止息屏及去掉导航栏

--- app/src/main/java/com/ang/androidcodeblock/MainActivity.kt
+++ app/src/main/java/com/ang/androidcodeblock/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ang.androidcodeblock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //设置屏幕常亮，有三种实现方式，这里只是其中一种;也就是阻止息屏！
+        window.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/ang/androidcodeblock/MyAPP.kt
@@ -0,0 +1,40 @@
+package com.ang.androidcodeblock
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyAPP(contentLayoutId: Int) : Application(), Application.ActivityLifecycleCallbacks {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        TODO("Not yet implemented")
+    }
+}
